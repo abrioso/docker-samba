8f0301425a49dd99a47cfb0d978336fc9bbf264b
Duc Anh Babim <ducanh.babim@yahoo.com>
2016-11-29 10:19:06 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
 # value
+SHARECONFIG=/share/smb.conf
+
 USER1=${USER:-samba}
 PASS=${PASS:-samba}
+UID=${UID:-1000}
+GID=${GID:-1000}
 WORKGROUP=${WORKGROUP:-WORKGROUP}
 HOSTNAME=$(hostname -s)
 PUBLICFOLDER1=${PUBLICFOLDER1:-data}
-PUBLICNAME1=${PUBLICSHARE1:-$PUBLICFOLDER1}
+PUBLICNAME1=${PUBLICNAME1:-$PUBLICFOLDER1}
 PRIVATEFOLDER1=${PRIVATEFOLDER1:-private}
 PRIVATENAME1=${PRIVATENAME1:-$PRIVATEFOLDER1}
 
-#PUBLICFOLDER2=${PUBLICFOLDER2:-data}
+#PUBLICFOLDER2=${PUBLICFOLDER2:-data2}
 #PUBLICNAME2=${PUBLICNAME2:-$PUBLICFOLDER2}
-#PRIVATEFOLDER2=${PRIVATEFOLDER2:-private}
+#PRIVATEFOLDER2=${PRIVATEFOLDER2:-private2}
 #PRIVATENAME2=${PRIVATENAME2:-$PRIVATEFOLDER2}
-#PUBLICFOLDER3=${PUBLICFOLDER3:-data}
+#PUBLICFOLDER3=${PUBLICFOLDER3:-data3}
 #PUBLICNAME3=${PUBLICNAME3:-$PUBLICFOLDER3}
-#PRIVATEFOLDER3=${PRIVATEFOLDER3:-private}
+#PRIVATEFOLDER3=${PRIVATEFOLDER3:-private3}
 #PRIVATENAME3=${PRIVATENAME3:-$PRIVATEFOLDER3}
-#PUBLICFOLDER4=${PUBLICFOLDER4:-data}
+#PUBLICFOLDER4=${PUBLICFOLDER4:-data4}
 #PUBLICNAME4=${PUBLICNAME4:-$PUBLICFOLDER4}
-#PRIVATEFOLDER4=${PRIVATEFOLDER4:-private}
+#PRIVATEFOLDER4=${PRIVATEFOLDER4:-private4}
 #PRIVATENAME4=${PRIVATENAME4:-$PRIVATEFOLDER4}
-
-if [ ! -D "/share/$PUBLICFOLDER" ]; then mkdir -p /share/$PUBLICFOLDER; fi
-
-# add a non-root user and group called "samba" with no password, no home dir, no shell, and gid/uid set to 1000
-RUN addgroup -g 1000 $USER1 && adduser -D -H -G $USER1 -s /bin/false -u 1000 $USER1
-
-# create a samba user matching our user from above with a very simple password ("samba")
-RUN echo -e "$PASS\n$PASS" | smbpasswd -a -s -c /config/smb.conf $USER1
 
 # set config supervisord
 if [ ! -f "/config/supervisord.conf" ]; then
@@ -38,13 +34,13 @@ nodaemon=true
 loglevel=info
 # set some defaults and start samba in foreground (-F), logging to stdout (-S), and using our config (-s path)
 [program:smbd]
-command=smbd -F -S -s /config/smb.conf
+command=smbd -F -S -s $SHARECONFIG
 stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 stderr_logfile=/dev/stderr
 stderr_logfile_maxbytes=0
 [program:nmbd]
-command=nmbd -F -S -s /config/smb.conf
+command=nmbd -F -S -s $SHARECONFIG
 stdout_logfile=/dev/stdout
 stdout_logfile_maxbytes=0
 stderr_logfile=/dev/stderr
@@ -53,8 +49,8 @@ EOF
 fi
 
 # set config samba
-if [ ! -f "/config/smb.conf" ]; then
-cat <<EOF>> /config/smb.conf
+if [ ! -f "$SHARECONFIG" ]; then
+cat <<EOF>> $SHARECONFIG
 [global]
     netbios name = $HOSTNAME
     workgroup = $WORKGROUP
@@ -68,10 +64,14 @@ cat <<EOF>> /config/smb.conf
     printing = bsd
     printcap name = /dev/null
     disable spoolss = yes
+EOF
 
-[$PUBLICNAME]
-    comment = $PUBLICNAME public folder
-    path = /share/$PUBLICFOLDER
+if [[ ! -z "${PUBLICFOLDER1}" ]]; then
+if [ ! -d "/share/$PUBLICFOLDER1" ]; then mkdir -p /share/$PUBLICFOLDER1 && chown -R $UID:$GID /share/$PUBLICFOLDER1; fi
+cat <<EOF>> $SHARECONFIG
+[$PUBLICNAME1]
+    comment = $PUBLICNAME1 public folder
+    path = /share/$PUBLICFOLDER1
     read only = yes
     write list = $USER1
     guest ok = yes
@@ -79,64 +79,52 @@ cat <<EOF>> /config/smb.conf
     veto files = /._*/.DS_Store/
     delete veto files = yes
 EOF
-
+fi
 if [[ ! -z "${PRIVATEFOLDER1}" ]]; then
-if [ ! -D "/share/$PRIVATEFOLDER1" ]; then mkdir -p /share/$PRIVATEFOLDER1; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PRIVATEFOLDER1" ]; then mkdir -p /share/$PRIVATEFOLDER1 && chown -R $UID:$GID /share/$PRIVATEFOLDER1; fi
+cat <<EOF>> $SHARECONFIG
 [$PRIVATENAME1]
     comment = $PRIVATENAME1 private folder
     path = /share/$PRIVATEFOLDER1
     writeable = yes
     valid users = $USER1
-    # getting rid of those annoying .DS_Store files created by Mac users...
-    veto files = /._*/.DS_Store/
-    delete veto files = yes
 EOF
 fi
 
 if [[ ! -z "${PRIVATEFOLDER2}" ]]; then
-if [ ! -D "/share/$PRIVATEFOLDER2" ]; then mkdir -p /share/$PRIVATEFOLDER2; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PRIVATEFOLDER2" ]; then mkdir -p /share/$PRIVATEFOLDER2 && chown -R $UID:$GID /share/$PRIVATEFOLDER2; fi
+cat <<EOF>> $SHARECONFIG
 [$PRIVATENAME2]
     comment = $PRIVATENAME2 private folder
     path = /share/$PRIVATEFOLDER2
     writeable = yes
     valid users = $USER1
-    # getting rid of those annoying .DS_Store files created by Mac users...
-    veto files = /._*/.DS_Store/
-    delete veto files = yes
 EOF
 fi
 if [[ ! -z "${PRIVATEFOLDER3}" ]]; then
-if [ ! -D "/share/$PRIVATEFOLDER3" ]; then mkdir -p /share/$PRIVATEFOLDER3; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PRIVATEFOLDER3" ]; then mkdir -p /share/$PRIVATEFOLDER3 && chown -R $UID:$GID /share/$PRIVATEFOLDER3; fi
+cat <<EOF>> $SHARECONFIG
 [$PRIVATENAME3]
     comment = $PRIVATENAME3 private folder
     path = /share/$PRIVATEFOLDER3
     writeable = yes
     valid users = $USER1
-    # getting rid of those annoying .DS_Store files created by Mac users...
-    veto files = /._*/.DS_Store/
-    delete veto files = yes
 EOF
 fi
 if [[ ! -z "${PRIVATEFOLDER4}" ]]; then
-if [ ! -D "/share/$PRIVATEFOLDER4" ]; then mkdir -p /share/$PRIVATEFOLDER4; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PRIVATEFOLDER4" ]; then mkdir -p /share/$PRIVATEFOLDER4 && chown -R $UID:$GID /share/$PRIVATEFOLDER4; fi
+cat <<EOF>> $SHARECONFIG
 [$PRIVATENAME4]
     comment = $PRIVATENAME4 private folder
     path = /share/$PRIVATEFOLDER4
     writeable = yes
     valid users = $USER1
-    # getting rid of those annoying .DS_Store files created by Mac users...
-    veto files = /._*/.DS_Store/
-    delete veto files = yes
 EOF
 fi
 
 if [[ ! -z "${PUBLICFOLDER2}" ]]; then
-if [ ! -D "/share/$PUBLICFOLDER2" ]; then mkdir -p /share/$PUBLICFOLDER2; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PUBLICFOLDER2" ]; then mkdir -p /share/$PUBLICFOLDER2 && chown -R $UID:$GID /share/$PUBLICFOLDER2; fi
+cat <<EOF>> $SHARECONFIG
 [$PUBLICNAME2]
     comment = $PUBLICNAME2 public folder
     path = /share/$PUBLICFOLDER2
@@ -149,8 +137,8 @@ cat <<EOF>> /config/smb.conf
 EOF
 fi
 if [[ ! -z "${PUBLICFOLDER3}" ]]; then
-if [ ! -D "/share/$PUBLICFOLDER3" ]; then mkdir -p /share/$PUBLICFOLDER2; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PUBLICFOLDER3" ]; then mkdir -p /share/$PUBLICFOLDER3 && chown -R $UID:$GID /share/$PUBLICFOLDER3; fi
+cat <<EOF>> $SHARECONFIG
 [$PUBLICNAME3]
     comment = $PUBLICNAME3 public folder
     path = /share/$PUBLICFOLDER3
@@ -163,8 +151,8 @@ cat <<EOF>> /config/smb.conf
 EOF
 fi
 if [[ ! -z "${PUBLICFOLDER4}" ]]; then
-if [ ! -D "/share/$PUBLICFOLDER4" ]; then mkdir -p /share/$PUBLICFOLDER4; fi
-cat <<EOF>> /config/smb.conf
+if [ ! -d "/share/$PUBLICFOLDER4" ]; then mkdir -p /share/$PUBLICFOLDER4 && chown -R $UID:$GID /share/$PUBLICFOLDER4; fi
+cat <<EOF>> $SHARECONFIG
 [$PUBLICNAME4]
     comment = $PUBLICNAME4 public folder
     path = /share/$PUBLICFOLDER4
@@ -176,6 +164,12 @@ cat <<EOF>> /config/smb.conf
     delete veto files = yes
 EOF
 fi
+
+# add a non-root user and group called "samba" with no password, no home dir, no shell, and gid/uid set to 1000
+addgroup -g 1000 $USER1 && adduser -D -H -G $USER1 -s /bin/false -u 1000 $USER1
+
+# create a samba user matching our user from above with a very simple password ("samba")
+echo -e "$PASS\n$PASS" | smbpasswd -a -s -c $SHARECONFIG $USER1
 
 fi
 
